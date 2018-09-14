bb59014bb3a15e3374cb0dad6ab23b2daaad2d80
Novaki <Novaki@10.20.2.206>
2018-09-14 20:56:44 -0300

    Atualizado

---
+++ IntegradorAdivinha/src/Classes/Pergunta.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class Pergunta {
+
+	private String pergunta;
+	
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+	
+	public String getPergunta() {
+		return pergunta;
+	}
+	
+}
